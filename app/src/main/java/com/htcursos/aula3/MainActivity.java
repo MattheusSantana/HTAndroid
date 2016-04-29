04c81dc565e8995387658678527e3ea74c6b9b2a
MattheusSantana <matheusess@hotmail.com>
2016-04-29 16:37:41 -0300

    Implementando operadores Aritmeticos

--- app/src/main/java/com/htcursos/aula3/MainActivity.java
+++ app/src/main/java/com/htcursos/aula3/MainActivity.java
@@ -1,6 +1,7 @@
 package com.htcursos.aula3;
 
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
@@ -18,11 +19,17 @@ import butterknife.OnClick;
  */
 public class MainActivity extends AppCompatActivity {
 
+    double[] vetor = new double[2];
+    double result = 0;
+    boolean soma = false;
+    boolean sub = false;
+    boolean div = false;
+    boolean mult = false;
+    boolean virg = false;
+    boolean resultado = false;
+
     @Bind(R.id.texto)
     TextView texto;
-
-    @Bind(R.id.resultado)
-    TextView resultado;
 
     @Bind(R.id.bt1)
     Button bt1;
@@ -76,7 +83,6 @@ public class MainActivity extends AppCompatActivity {
     Button btDel;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -87,108 +93,193 @@ public class MainActivity extends AppCompatActivity {
 
 
     @OnClick(R.id.bt_del)
-    public void click(){
+    public void click() {
         texto.setText("");
+        if(resultado == true){
+            result = 0;
+        }
 
     }
 
 
-    public void txt(String valor){
-
-        texto.setText(texto.getText()+""+valor);
+    public void txt(String valor) {
+        texto.setText(texto.getText() + "" + valor);
+        if (soma == true || sub == true || div == true || mult == true) {
+            vetor[1] = Double.parseDouble(texto.getText().toString());
+        }
 
     }
+
 
     @OnClick(R.id.bt1)
-    public void bt1(){
+    public void bt1() {
         txt("1");
+
     }
 
     @OnClick(R.id.bt2)
-    public void bt2(){
+    public void bt2() {
         txt("2");
 
     }
 
     @OnClick(R.id.bt3)
-    public void bt3(){
+    public void bt3() {
         txt("3");
 
     }
 
     @OnClick(R.id.bt4)
-    public void bt4(){
+    public void bt4() {
         txt("4");
 
     }
 
     @OnClick(R.id.bt5)
-    public void bt5(){
+    public void bt5() {
         txt("5");
+
     }
 
 
     @OnClick(R.id.bt6)
-    public void bt6(){
+    public void bt6() {
         txt("6");
+
     }
 
 
     @OnClick(R.id.bt7)
-    public void bt7(){
+    public void bt7() {
         txt("7");
+
 
     }
 
 
     @OnClick(R.id.bt8)
-    public void bt8(){
+    public void bt8() {
         txt("8");
+
     }
 
 
     @OnClick(R.id.bt9)
-    public void bt9(){
+    public void bt9() {
         txt("9");
+
     }
 
 
     @OnClick(R.id.bt0)
-    public void bt0(){
+    public void bt0() {
         txt("0");
+
     }
 
 
     @OnClick(R.id.bt_virgula)
-    public void btVirgula(){
-        txt(",");
+    public void btVirgula() {
+        txt(".");
+        virg = true;
+        if (virg == true) {
+            return;
+        }
+
     }
 
 
     @OnClick(R.id.bt_soma)
-    public void btSoma(){
-        txt("+");
+    public void btSoma() {
+        try{
+            if (texto.getText()== ""){
+                btSoma.setEnabled(false);
+            }
+
+        vetor[0] = Double.parseDouble(texto.getText().toString());
+        texto.setText("");
+        soma = true;
+        virg = false;
+    }catch (Exception e){
+        texto.setText("");
+    }
 
     }
 
 
     @OnClick(R.id.bt_dividir)
-    public void btDividir(){
-        txt("÷");
-
-
+    public void btDividir() {
+        try {
+            if (texto.getText()== ""){
+                btDividir.setEnabled(false);
+            }
+            vetor[0] = Double.parseDouble(texto.getText().toString());
+            texto.setText("");
+            div = true;
+            virg = false;
+        }catch (Exception e){
+            texto.setText("");
+        }
     }
 
 
     @OnClick(R.id.bt_subtracao)
-    public void btSubtracao(){
-        txt("-");
+    public void btSubtracao() {
+        try{
+            if (texto.getText()== ""){
+                btSubtracao.setEnabled(false);
+            }
+        vetor[0] = Double.parseDouble(texto.getText().toString());
+        texto.setText("");
+        sub = true;
+        virg = false;
+        }catch (Exception e){
+            texto.setText("");
+        }
+
     }
 
 
     @OnClick(R.id.bt_multiplicacao)
-    public void btMultiplicacao(){
-        txt("x");
+    public void btMultiplicacao() {
+        try{
+            if (texto.getText()== ""){
+                btMultiplicacao.setEnabled(false);
+            }
+        vetor[0] = Double.parseDouble(texto.getText().toString());
+        texto.setText("");
+        mult = true;
+        virg = false;
+        }catch (Exception e){
+            texto.setText("");
+        }
+
+
     }
 
+    @OnClick(R.id.bt_igual)
+    public void btResultado() {
+        resultado = true;
+
+        if (soma == true) {
+            result = vetor[0] + vetor[1];
+        } else if (sub == true) {
+            result = vetor[0] - vetor[1];
+        } else if (div == true) {
+            result = vetor[0] / vetor[1];
+        } else if (mult == true) {
+            result = vetor[0] * vetor[1];
+        }
+
+        texto.setText(String.valueOf(result));
+
+        soma = false;
+        sub = false;
+        div = false;
+        mult = false;
+        virg = false;
+        vetor[0]= 0;
+        vetor[1]=0;
+
+    }
 }
---
+++ app/src/main/java/com/htcursos/aula3/test.java
@@ -0,0 +1,9 @@
+package com.htcursos.aula3;
+
+/**
+ * Created by Matheus on 26/04/2016.
+ */
+public class test {
+
+
+}
